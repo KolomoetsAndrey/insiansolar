c292ff2809b17d74d1dd1a138eda0bcd13860eeb
Andrey Kolomoets <kolomoets.and@gmail.com>
2019-03-19 10:29:42 +0200

    index

---
+++ js/tooltip.js
@@ -0,0 +1,31 @@
+document.onmousemove = moveTip;
+function moveTip(e) {
+  floatTipStyle = document.getElementById("floatTip").style;
+  w = 150;
+
+  if (document.all)  { 
+    x = event.clientX + document.body.scrollLeft; 
+    y = event.clientY + document.body.scrollTop; 
+
+  } else   { 
+    x = e.pageX;
+    y = e.pageY;
+  }
+
+  if ((x + w + 10) < document.body.clientWidth) { 
+    floatTipStyle.left = x + 'px';
+
+  } else { 
+    floatTipStyle.left = x - w + 'px';
+  }
+}
+
+function toolTip(msg) {
+  floatTipStyle = document.getElementById("floatTip").style;
+  if (msg) {
+    document.getElementById("floatTip").innerHTML = msg;
+    floatTipStyle.display = "block";
+  } else { 
+    floatTipStyle.display = "none";
+  } 
+}
